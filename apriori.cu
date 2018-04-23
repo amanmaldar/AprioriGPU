03969611fcf2243c76327cb36179a79dc582d6c3
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-22 22:10:14 -0400

    end of day - sunday

--- apriori.cu
+++ apriori.cu
@@ -20,43 +20,33 @@ Data: (6entries.txt)
 #include "functions.hcu"
 
 
-double minSupp = 0.001; // 0.001;
-
-
-
+//double minSupp = 0.001; // 0.001;
 
 
 __shared__ int smem[128];
-//__shared__ int sum[9];
 
 __global__ void prefix_scan_kernel (int *A_device, int *B_device , int *ans_device) {
-//__global__ void prefix_scan_kernel (int *globalDataset_device, int *globalDatasetThreadIndex_device, int *ans_device) {
 
 	int tid = threadIdx.x;
-	//sum[tid] = 0;
-	__syncthreads(); 	//wait for all threads
+	__syncthreads(); 	
     
 	int index1=0;
 	int sum = 0;
 	int begin = B_device[tid];
 	while (tid < 9){
 		printf("tid: %d begin: %d A_device[begin]: %d \n", tid, begin,A_device[begin]);
-		//printf("tid: %d  A_device[begin+index1]: %d  begin: %d \n", tid, A_device[begin+index1], begin);
 		while (A_device[begin+index1] != -1){
-			//printf("\n A_device[begin+index1]: %d \n", A_device[begin+index1]);
+		// map data from A_device to smem // limitation on smem comes in picture
 			smem[begin+index1] = A_device[begin+index1];
-			__syncthreads(); 	//wait for all threads
+			__syncthreads(); 	
 			
 			index1++;
 		}
 		printf("index1: %d \n", index1);
-		//printf("tid: %d sum[tid]:",tid);
 		for (int i=begin;i<begin+index1;i++){
 			sum += smem[i];
-			//printf(" %d ", i);
-			__syncthreads(); 	//wait for all threads
+			__syncthreads(); 	
 		}
-		//ans_device[threadIdx.x] = sum;
 		ans_device[tid] = sum;
 		tid+=9;
 	}
@@ -71,47 +61,44 @@ void Execute(int argc){
 
 	vector <int> A; //= globalDataset     // convert itemId_TidMapping into long array
     vector <int> B ; // = globalDatasetThreadIndex;
-    //int *globalDatasetCpu = (int *) malloc (sizeof(int)* totalItems);
     int *A_cpu = (int *) malloc (sizeof(int)* totalItems);
     int *B_cpu = (int *) malloc (sizeof(int)* (maxItemID+1));    //index array lenght same as number of items
 	int *ans_cpu = (int *) malloc (sizeof(int)* (maxItemID+1));
 	
     int k =0;                   // global pointer for globalMap
-    //globalDatasetThreadIndex.push_back(k);
 	for(int i=0;i<=maxItemID;i++){
-        B.push_back(k);
+        //B.push_back(k);
         B_cpu[i] = k;
         vector <int> tmp11 = itemId_TidMapping[i];    // copy entire vector
         for(int j=1;j<tmp11.size();j++){ // last item should be inclusive, first element is excluded
-            A.push_back(tmp11[j]);
+           // A.push_back(tmp11[j]);
             A_cpu[k] = tmp11[j];
-            //globalDatasetCpu[k] = globalDataset[k];
             k++;
 		}
 
-        A.push_back(-1);    // seperate mappings by -1
+        //A.push_back(-1);    // seperate mappings by -1
         A_cpu[k] = -1;
         k++;
-       // globalDatasetThreadIndex.push_back(k);
-	}
+
+		}
 /*	cout << " Printing itemId_TidMapping as array: " << endl;
     for(int i =0;i<A.size();i++){
         //A_cpu[i] = A[i];
         cout << A[i] << " " ;
     }cout << endl;*/
-    cout << " Printing itemId_TidMapping copy: " << totalItems << endl;
+	
+    cout << " Printing itemId_TidMapping copy A_cpu: " << totalItems << endl;
     for(int i =0;i<totalItems;i++){
         cout << A_cpu[i] << " " ;
     }cout << endl;
-	cout << A_cpu[7] << " " << A_cpu[8] << " " << A_cpu[9] << " tesing \n " ;
 
  /*   cout << " Printing starting indexes " << endl;
     for(int i =0;i<B.size();i++){
         cout << B[i] << " " ;
     }cout << endl;*/
 
-    cout << " Printing starting indexes " << endl;
-    for(int i =0;i<B.size();i++){
+    cout << " Printing starting indexes B_cpu: " << endl;
+    for(int i =0;i<= maxItemID;i++){
         cout << B_cpu[i] << " " ;
     }cout << endl;
 	
@@ -148,12 +135,7 @@ void Execute(int argc){
 		cout << ans_cpu[i] << " ";
 	} cout << endl;
  
-    //cout << "two_freq_itemset:      " << two_freq_itemset << endl << "\n";
 
-    //******************************************************************************************************************
-
-    
-    //work till pair of 2
     return;
     
    
