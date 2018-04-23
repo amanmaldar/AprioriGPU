9f17111da9ca6776982d46dc0f580f0bd58546b6
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-22 20:47:26 -0400

    a

--- apriori.cu
+++ apriori.cu
@@ -36,12 +36,11 @@ __global__ void prefix_scan_kernel (int *A_device, int *B_device , int *ans_devi
     
 	int index=0;
 	int sum = 0;
-	//int tmp = 
 	int begin = B_device[threadIdx.x];
 	while (tid < 9){
 		printf("tid: %d begin: %d\n", tid, begin);
 		//printf("tid: %d  A_device[begin+index]: %d  begin: %d \n", tid, A_device[begin+index], begin);
-		while (A_device[begin+index] != -1){
+	/*	while (A_device[begin+index] != -1){
 			//printf("\n A_device[begin+index]: \n", A_device[begin+index]);
 			smem[index] = A_device[begin+index];
 			index++;
@@ -49,10 +48,10 @@ __global__ void prefix_scan_kernel (int *A_device, int *B_device , int *ans_devi
 		
 		for (int i=0;i<index;i++){
 			sum+= smem[index];
-		}
+		}*/
 		//ans_device[threadIdx.x] = sum;
-		ans_device[threadIdx.x] = begin;
-		
+		ans_device[tid] = begin;
+		tid+=9;
 	}
 } // end kernel function
 
