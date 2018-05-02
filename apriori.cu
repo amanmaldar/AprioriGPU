07c13761b2742b51b4f7a7b6f06af9d69c8f5a7a
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-05-02 19:14:15 -0400

    Update apriori.cu

--- apriori.cu
+++ apriori.cu
@@ -106,58 +106,64 @@ for (int i = 0; i < len_p; i++)
 
 void Execute(int argc){
 
-    	parse_database(argc);
+    parse_database(argc);
 
-	vector <int> A; //= globalDataset     // convert itemId_TidMapping into long array
-    vector <int> B ; // = globalDatasetThreadIndex;
     int *A_cpu = (int *) malloc (sizeof(int)* totalItems);
     int *B_cpu = (int *) malloc (sizeof(int)* (maxItemID+1));    //index array lenght same as number of items
 	int *ans_cpu = (int *) malloc (sizeof(int)* (maxItemID+1));
 	
-    int k =0;                   // global pointer for globalMap
-	for(int i=0;i<=maxItemID;i++){
-        //B.push_back(k);
+	//---------------This section processes the variables that should be transferred to GPU memory as global database--------
+	// TIP - gloabal Map should contain all the itemIds, even if they are not frequent, we need them to have correct mapping
+    int k =0;                  						 // global pointer for globalMap
+	for(int i=0;i<=maxItemID;i++) {
         B_cpu[i] = k;
         vector <int> tmp11 = itemId_TidMapping[i];    // copy entire vector
-        for(int j=1;j<tmp11.size();j++){ // last item should be inclusive, first element is excluded
-           // A.push_back(tmp11[j]);
+        for(int j=1;j<tmp11.size();j++) {			  // last item should be inclusive, first element is excluded
             A_cpu[k] = tmp11[j];
             k++;
 		}
-
-        //A.push_back(-1);    // seperate mappings by -1
-        A_cpu[k] = -1;
+		A_cpu[k] = -1;								// seperate mappings by -1
         k++;
 
-		}
-/*	cout << " Printing itemId_TidMapping as array: " << endl;
-    for(int i =0;i<A.size();i++){
-        //A_cpu[i] = A[i];
-        cout << A[i] << " " ;
-    }cout << endl;*/
-	
-    cout << " Printing itemId_TidMapping copy A_cpu: " << totalItems << endl;
-    for(int i =0;i<totalItems;i++){
-        cout << A_cpu[i] << " " ;
-    }cout << endl;
+	}
 
- /*   cout << " Printing starting indexes " << endl;
-    for(int i =0;i<B.size();i++){
-        cout << B[i] << " " ;
-    }cout << endl;*/
+    cout << " Printing itemId_TidMapping copy A_cpu: " << totalItems << endl;
+    for(int i =0;i<totalItems;i++) {
+        cout << A_cpu[i] << " " ;
+    } cout << endl;
+
 
     cout << " Printing starting indexes B_cpu: " << endl;
-    for(int i =0;i<= maxItemID;i++){
+    for(int i =0;i<= maxItemID;i++) {
         cout << B_cpu[i] << " " ;
-    }cout << endl;
+    } cout << endl;
+	//-----------------------------------------------------------------------------------------------------------
 	
-	//return;
 	
+	//-----------Generates single frequent items. Used later to create pairs-------------------------------------
+    L1.push_back(0);    // initialized first index with 0 as we are not using it.
+    minSupport = 1;
+    // Following code generates single items which have support greater than min_sup
+    // compare the occurrence of the object against minSupport
 
-	//vector <int> A; //= globalDataset     // convert itemId_TidMapping into long array
-    //vector <int> B ; // = globalDatasetThreadIndex;
-	//int *A_cpu = globalDataset
-    //int *B_cpu = globalDatasetThreadIndex
+    cout << "\n Support:" << minSupport << endl << "\n";
+    //Generate L1 - filtered single items ? I think this should be C1, not L1.
+
+    for (int i=0; i<= maxItemID; i++)
+    {
+        if(itemIDcount[i] >= minSupport){
+            L1.push_back(i);     //push TID into frequentItem
+            one_freq_itemset++;
+            cout << "1 Frequent Item is: (" << i << ") Freq is: " << itemIDcount[i] << endl;
+        }
+    }
+    //cout << "one_freq_itemset:      " << one_freq_itemset << endl << "\n";
+	//-----------------------------------------------------------------------------------------------------------
+	
+	//----------------This section generates the pair of 2-------------------------------------------------------
+	
+	//-----------------------------------------------------------------------------------------------------------
+	
 	
 	int *A_device; //device storage pointers
 	int *B_device;
@@ -168,17 +174,7 @@ void Execute(int argc){
     cudaMalloc ((void **) &B_device, sizeof (int) * 9);
     cudaMalloc ((void **) &ans_device, sizeof (int) * 9);
 
-/*
-    cudaMemcpy (A_device, A_cpu, sizeof (int) * totalItems, cudaMemcpyHostToDevice);
-    cudaMemcpy (B_device, B_cpu, sizeof (int) * 9, cudaMemcpyHostToDevice);
-	cudaMemcpy (ans_device, ans_cpu, sizeof (int) * 9, cudaMemcpyHostToDevice);
 
-	int numberOfBlocks = 1;
-	int threadsInBlock = 100;
-	
-	addition_scan_kernel <<< numberOfBlocks,threadsInBlock >>> (A_device, B_device, ans_device);
-    cudaMemcpy (ans_cpu, ans_device, sizeof (int) * 9, cudaMemcpyDeviceToHost);
-*/
 	int *p_cpu = (int *) malloc (sizeof(int));
 	int *q_cpu = (int *) malloc (sizeof(int));
 	int *common_cpu = (int *) malloc (sizeof(int));
