9dcd5e43c7239c234733212d94c42fc2ad9b500e
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-05-06 00:11:03 -0400

    Update apriori.cu

--- apriori.cu
+++ apriori.cu
@@ -222,8 +222,8 @@ for (int i = 0; i < len_p; i++)
 
 
 void Execute(int argc, char *prnt){
-
-	printing = int(*prnt); 	//decide printing actions
+	cout << *prnt << prnt << int(prnt) << endl;
+	//printing = int(*prnt); 	//decide printing actions
 	// Generate C1. Parsing the database generates C1.
 	auto parse_start = chrono::high_resolution_clock::now();
   	parse_database(argc);
